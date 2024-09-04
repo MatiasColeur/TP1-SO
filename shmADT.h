f7608d2a63482c4d133b1d72192af9773c4dcda1
MaxiChiate <chiatellinomaximo@gmail.com>
2024-09-04 12:20:55 -0300

    shmADT works
    It remains to add a function to beSafe.c to also kill shm when there is an error or something like that

--- shmADT.h
+++ shmADT.h
@@ -3,24 +3,43 @@
 #ifndef SHM_ADT_H
 #define SHM_ADT_H
 	
-#include "heap.h"
-#include <stdlib.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <unistd.h>
-#include <semaphore.h>
-#include <sys/stat.h>	
-
-typedef struct sharedCDT * sharedADT;
-
-sharedADT createShm(char * name, size_t size);
-
-void killShared(sharedADT shm);
+	#include "heap.h"
+	#include <stdlib.h>
+	#include <fcntl.h>
+	#include <sys/mman.h>
+	#include <unistd.h>
+	#include <semaphore.h>
+	#include <sys/stat.h>	
+	#include <string.h>
 
 
+	typedef struct sharedCDT * sharedADT;
+
+
+//createShm(): Returns an instance of a sharedADT type, coding it shared memory with @name, having @size space avialiable.
+
+	sharedADT createShm(const char * name, size_t size);
 
 
 
+//killShared(): Deletes an instance of a sharedADT type. It handles the dynamic memory and files in uses.
 
+	void killShm(sharedADT shm);
+
+
+	
+//writeShm(): Writes @src in the shared memory. It doesn't delete the previous information written in the shared memory. @size must fit in the remining space in the shared memory.
+
+//	@return the exact space avialiable in the shared memory. Is on the user translate this information to the data type @src is using.
+
+	size_t writeShm(sharedADT shm, const void * src, size_t size);
+
+
+
+//readShm(): Consume @size content from the shared memory and writes it into @target.
+
+//	@return the exact space avialiable in the shared memory. Is on the user translate this information to the data type @src is using.
+
+	size_t readShm(sharedADT shm, void * target, size_t size);
 
 #endif
--- shmCDT.c
+++ shmCDT.c
@@ -4,78 +4,177 @@
 
 struct sharedCDT {
 
-	int shm_fd;
+	int shmFd;
 	void * mapped;
 	char * name;
 	size_t size;
-	sem_t * semaphore;
-
+	size_t using;
+	sem_t * semaphore;	
 }; 
 
-static void initSemaphore(sharedADT shm, char * sem_name) {
-	sem_t * sem = sem_open(sem_name, O_CREAT);
-	errorManagement(sem == SEM_FAILED, "sem_open");
+
+
+//-----------------------Spawn shmADT----------------------
+
+
+
+static void initSemaphore(sharedADT shm) {
+
+	sem_t * sem = sem_open(shm->name, O_CREAT, 0664, 1);
+	errorManagement(sem == SEM_FAILED, "shared memory open failed");
+
 	shm->semaphore = sem;
 }
 
 static void mapToMemory(sharedADT shm) {
-	errorManagement((shm->mapped = mmap(NULL, shm->size, PROT_READ | PROT_WRITE, MAP_SHARED, shm->shm_fd, 0)) == MAP_FAILED, "mmap failed");
+
+	errorManagement((shm->mapped = mmap(NULL, shm->size, PROT_READ | PROT_WRITE, MAP_SHARED, shm->shmFd, 0)) == MAP_FAILED, 
+		"memory map failed");
+}
+
+static void truncateFd(sharedADT shm)	{
+
+	errorManagement(ftruncate(shm->shmFd, shm->size) == -1, "ftruncate failed");
+}
+
+static void openShm(sharedADT shm)	{
+
+	shm->shmFd = shm_open(shm->name, O_CREAT | O_EXCL | O_RDWR, 0666);	
+	errorManagement( shm->shmFd == -1, "shared memory open failed");
 }
 
 
-sharedADT createShm(char * name, size_t size) {
+
+sharedADT createShm(const char * name, size_t size) {
+
 	sharedADT shm = safeMalloc(sizeof(struct sharedCDT));
 	shm->size = size;
-	shm->name = name;
+	shm->using = 0;
+	
+	shm->name = safeCalloc(strlen(name)+1,sizeof(name[0]));
+	strcat(shm->name, name);
 
-	errorManagement((shm->shm_fd = shm_open(name, O_CREAT | O_EXCL | O_RDWR, 0666)) == -1, "shm_open");
-	//errorManagement(ftruncate(shm->shm_fd, shm->size) == -1, "ftruncate");
-
-	if (ftruncate(shm->shm_fd, shm->size) == -1) {
-		perror("ftruncate");
-		close(shm->shm_fd);
-		free(shm);
-		return NULL;
-	}
-
-	initSemaphore(shm, name);
+	openShm(shm);
+	truncateFd(shm);
+	initSemaphore(shm);
 	mapToMemory(shm);
 
 	return shm;
 }
 
-static void unmap(sharedADT shm) {
-	errorManagement(munmap(shm->mapped, shm->size) == -1, "munmap");
+
+
+//-----------------------Kill shmADT----------------------
+
+
+
+static void unmapFromMemory(sharedADT shm) {
+	
+	if (shm->mapped != NULL) {
+
+		errorManagement(munmap(shm->mapped, shm->size) == -1, "memory unmap failed");
+	}
 }
 
-
-//TO do
-void killShared(sharedADT shm)	{
-	if (shm == NULL) {
-		return;
-	}
-	if (shm->mapped != NULL) {
-		unmap(shm);
-	}
-	errorManagement(close(shm->shm_fd) == -1, "close");
+static void killSemaphore(sharedADT shm)	{
 
 	if (shm->semaphore != NULL) {
-		errorManagement(sem_close(shm->semaphore) == -1, "close");
-		errorManagement(sem_unlink(shm->name) == -1, "close");
+
+		errorManagement(sem_close(shm->semaphore) == -1, "memory shared close failed");
+		errorManagement(sem_unlink(shm->name) == -1, "memory shared close failed");
 	}
+}
 
-	if (shm->name != NULL) {
-		free(shm->name);
-	}
-
-	unmap(shm);
-
+static void closeShm(sharedADT shm)	{
+	
+	errorManagement(shm_unlink(shm->name) == -1, "memory shared close failed");
+	errorManagement(close(shm->shmFd) == -1, "shared memory close failed");
 }
 
 
 
-// int main() {
-// 	sharedADT shm = createShm("Lo que más te haga feliz en esta vida", 1024);
-// 	killShared(shm);
-// killHeapMonitor();
-// }
+void killShm(sharedADT shm)	{
+
+	if (shm == NULL) {
+
+		return;
+	}
+
+	unmapFromMemory(shm);
+	closeShm(shm);
+	killSemaphore(shm);
+
+	//To free shm->name and shm, needs to add features to heap.c
+}
+
+
+
+//-----------------------Read/write----------------------
+
+
+
+size_t writeShm(sharedADT shm, const void * src, size_t size)	{
+
+	if(shm == NULL) return 0;
+	if(src == NULL) return 0;
+
+//fix:
+	if( (shm->size - shm->using) < size)	{	
+	
+		killShm(shm);
+		errorManagement( 1, "write shm failed");
+	}
+	
+	sem_wait(shm->semaphore);
+	
+		memcpy(shm->mapped + shm->using, src, size); 
+
+	sem_post(shm->semaphore);
+
+	return shm->using += size;
+}
+
+
+
+size_t readShm(sharedADT shm, void * target, size_t size)	{
+
+	if(shm == NULL) return 0;
+	if(target == NULL) return 0;
+
+//fix:
+	if(shm->using < size)	{
+			
+		killShm(shm);
+		errorManagement(1, "read shm failed");
+	}
+
+	sem_wait(shm->semaphore);
+	
+		memcpy(target, shm->mapped, size);
+		memcpy(shm->mapped, shm->mapped+size, shm->using-size);
+
+	sem_post(shm->semaphore);
+
+	return shm->using -= size;
+	
+}
+
+
+int main() {
+
+	sharedADT shm = createShm("Lo que más te haga feliz en esta vida", 1024);
+	
+	char * s = "Hola\n";
+	int dim = strlen(s)+1;
+
+	writeShm(shm, s, dim*sizeof(s[0]));
+
+	char * t = safeMalloc(dim*sizeof(s[0]));
+
+	readShm(shm, t, dim*sizeof(s[0]));
+
+	printf(t);
+
+ 	killShm(shm);
+	killHeapMonitor();
+}
