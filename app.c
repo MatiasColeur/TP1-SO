5bcc50f2dad363f96f9adb2331446c08377cab81
MatiasColeur <mcoleur@itba.edu.ar>
2024-09-12 22:20:55 +0000

    add slave_monitor struct, getOneSlave() function and closePipes() function

--- app.c
+++ app.c
@@ -11,65 +11,84 @@
 #define SLAVE_ARGV {CHILD_PATH,NULL}
 #define SLAVE_ENVP {NULL}
 
+typedef struct 
+{
+	int * pipe_fd_in[SLAVES_AMOUNT];		//File descriptors from pipes from each slave [read,write]
+	int * pipe_fd_out[SLAVES_AMOUNT];
 
-static pid_t * getSlaves(const int slaves_amount);
+	pid_t slaves[SLAVES_AMOUNT];
+
+	int total_slaves;
+
+} slave_monitor;
+
+
+
+
+static slave_monitor * getSlaves(const int slaves_amount);
+
+static void closePipes(slave_monitor * monitor);
+
+static void getOneSlave(slave_monitor * monitor,int slave_position);
+
+
+
 
 
 int main(int argc, char * argv[])	{
 
-	pid_t * slaves= getSlaves(10);
+	slave_monitor * monitor= getSlaves(10);
 
-	for(int i; i< SLAVES_AMOUNT; i++){
-
-		printf("pid: %d\n",slaves[i]);
-		
-	}
-
+	closePipes(monitor);
 	return 0;
 }
 
-static pid_t * getSlaves(const int files_amount)	{
+static slave_monitor * getSlaves(const int files_amount)	{
 
-	int max_slaves= SLAVES_AMOUNT;
-	pid_t * slaves;
+	slave_monitor * monitor= (slave_monitor *) malloc(sizeof(slave_monitor));
+	monitor->total_slaves=SLAVES_AMOUNT;
 
-
-	if (files_amount< SLAVES_AMOUNT)	{
-		
-		max_slaves= files_amount;
-	
+	for(int i=0; i< monitor->total_slaves;i++)	{
+		getOneSlave(monitor,i);
 	}
 
-	slaves = (pid_t *) malloc(max_slaves * sizeof(pid_t *));
+	return monitor;
+}
+
+
+static void closePipes(slave_monitor * monitor)	{
+
+	for(int i=0; i<monitor->total_slaves;i++){
+		close(monitor->pipe_fd_in[i][0]);
+		close(monitor->pipe_fd_in[i][1]);
+		close(monitor->pipe_fd_out[i][0]);
+		close(monitor->pipe_fd_out[i][1]);
+	}
+
+}
+
+static void getOneSlave(slave_monitor * monitor,int slave_position)	{
+	
 	char * child_argv[] = SLAVE_ARGV;					
 	char * child_envp[] = SLAVE_ENVP;
-	int * pipe_fd_in;
-	int * pipe_fd_out;
+	monitor->pipe_fd_in[slave_position] = safePipeD();				
+	monitor->pipe_fd_out[slave_position]= safePipeD();
+
+	pid_t pid= safeFork();
 	
-	for(int i=0; i<max_slaves;i++){
-
-		pipe_fd_in = safePipeD();				
-		pipe_fd_out= safePipeD();
-
-		pid_t pid= safeFork();
-		
-		if(pid==0)	{								//Slave process
+	if(pid==0)	{								//Slave process
 
 
-			dup2(pipe_fd_in[0],STDIN_FILENO);		//Change pipe filedescriptors to STDIN and STDOUT
-			dup2(pipe_fd_out[1],STDOUT_FILENO);
+		dup2(monitor->pipe_fd_in[slave_position][0],STDIN_FILENO);		//Change pipe filedescriptors to STDIN and STDOUT
+		dup2(monitor->pipe_fd_out[slave_position][1],STDOUT_FILENO);
 
-			close(pipe_fd_in[0]);					//Close pipe filedescriptors that wont be use anymore
-			close(pipe_fd_out[1]);
+		close(monitor->pipe_fd_in[slave_position][0]);					//Close pipe filedescriptors that wont be use anymore
+		close(monitor->pipe_fd_out[slave_position][1]);
 
-			safeExecve(CHILD_PATH,child_argv,child_envp);
-		
-		}
-		else{										//App process
-			slaves[i]= pid;
-		}
-
+		safeExecve(CHILD_PATH,child_argv,child_envp);
+	
 	}
-
-	return slaves;
+	else{										//App process
+		monitor->slaves[slave_position]= pid;
+	}
 }
