0ed71bf8c3b8e9a54d4d9a2fd80d52419d4c36b4
MatiasColeur <mcoleur@itba.edu.ar>
2024-09-13 03:20:39 +0000

    small changes in app.c

--- app.c
+++ app.c
@@ -13,8 +13,8 @@
 
 typedef struct 
 {
-	int * pipe_fd_in[SLAVES_AMOUNT];		//File descriptors from pipes from each slave [read,write]
-	int * pipe_fd_out[SLAVES_AMOUNT];
+	int pipe_fd_write[SLAVES_AMOUNT];		//File descriptors from pipes from each slave. Its seen from the perspective
+	int pipe_fd_read[SLAVES_AMOUNT];		// of the master (where master writes and reads)
 
 	pid_t slaves[SLAVES_AMOUNT];
 
@@ -38,7 +38,7 @@ static void getOneSlave(slave_monitor * monitor,int slave_position);
 int main(int argc, char * argv[])	{
 
 	slave_monitor * monitor= getSlaves(10);
-
+	sleep(5);
 	closePipes(monitor);
 	return 0;
 }
@@ -56,39 +56,46 @@ static slave_monitor * getSlaves(const int files_amount)	{
 }
 
 
-static void closePipes(slave_monitor * monitor)	{
-
-	for(int i=0; i<monitor->total_slaves;i++){
-		close(monitor->pipe_fd_in[i][0]);
-		close(monitor->pipe_fd_in[i][1]);
-		close(monitor->pipe_fd_out[i][0]);
-		close(monitor->pipe_fd_out[i][1]);
-	}
-
-}
 
 static void getOneSlave(slave_monitor * monitor,int slave_position)	{
 	
 	char * child_argv[] = SLAVE_ARGV;					
 	char * child_envp[] = SLAVE_ENVP;
-	monitor->pipe_fd_in[slave_position] = safePipeD();				
-	monitor->pipe_fd_out[slave_position]= safePipeD();
+	int * pipe_read=safePipeD();				//where master will read
+	int * pipe_write= safePipeD();				//where master will write
+
+	monitor->pipe_fd_read[slave_position]=pipe_read[0];
+	monitor->pipe_fd_write[slave_position]=pipe_write[1];
 
 	pid_t pid= safeFork();
 	
 	if(pid==0)	{								//Slave process
 
 
-		dup2(monitor->pipe_fd_in[slave_position][0],STDIN_FILENO);		//Change pipe filedescriptors to STDIN and STDOUT
-		dup2(monitor->pipe_fd_out[slave_position][1],STDOUT_FILENO);
+		dup2(pipe_write[0],STDIN_FILENO);		//Change pipe filedescriptors to STDIN and STDOUT
+		dup2(pipe_read[1],STDOUT_FILENO);
 
-		close(monitor->pipe_fd_in[slave_position][0]);					//Close pipe filedescriptors that wont be use anymore
-		close(monitor->pipe_fd_out[slave_position][1]);
-
+		close(pipe_write[0]);					//Close pipe filedescriptors that wont be used anymore
+		close(pipe_read[1]);
+		
+		close(pipe_read[0]);					//Close pipe filedescriptors that child wont use
+		close(pipe_write[1]);
 		safeExecve(CHILD_PATH,child_argv,child_envp);
 	
 	}
 	else{										//App process
 		monitor->slaves[slave_position]= pid;
+		close(pipe_write[0]);					//Close pipe filedescriptors that wont be used
+		close(pipe_read[1]);
 	}
+}
+
+
+static void closePipes(slave_monitor * monitor)	{
+
+	for(int i=0; i<monitor->total_slaves;i++){
+		close(monitor->pipe_fd_write[i]);
+		close(monitor->pipe_fd_read[i]);
+	}
+
 }
