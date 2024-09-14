e138f67f9a03775f9002ee9b958505d132a6b892
gonza-sharif2003 <gonzalocuri@gmail.com>
2024-09-14 11:01:10 -0300

    View.c

--- View.c
+++ View.c
@@ -4,42 +4,54 @@
 #include "heap.h"
 #include "shmADT.h"
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
-int main(int argc, char * argv[])	{
 
-//Gets the buffer:
-/*
-	char * name;
+int main(int argc, char * argv[]) {
 
-	if(argc != 1)	{
-
-		name = argv[1];
-	}
-	else	{
-
-		errorManagement(scanf("%s",name) == EOF, "scanf failed");
+	if (argc != 2) {
+		exit(EXIT_FAILURE);
 	}
 
-	sharedADT shm = createShm(name, 1024);
+	char * shmname = argv[1];
+    int fd;	
+	struct stat st;
 
-	char buf;
-	char toPrint[1024];
-	int i=0;
-	int shmSpaceAvailable;
+    fd = shm_open(shmname, O_RDWR, 0666);
+    if (fd == -1) {
+        perror("Error al abrir memoria compartida");
+        exit(EXIT_FAILURE);
+    }
 
-	while((shmSpaceAvailable = readShm(shm, &buf, 1)) > 1024 && buf != EOF)	{
-		
-		toPrint[i++] = buf;
-		
-		if(buf == '\n')	{
-			
-			printf("%s", toPrint);
-			i=0;
-		}
-		
-	}
-	
-	killShm(shm);
-	killHeapMonitor();
-*/
+	if (fstat(fd, &st) == -1) {
+        perror("Error al obtener tamaño de la memoria compartida");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+    size_t size = st.st_size;
+
+
+    void * mapped_mem = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (mapped_mem == MAP_FAILED) {
+        perror("Error al mapear memoria");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+	char * shmbuff = (char *) mapped_mem;
+
+	char * token = strtok(shmbuff, ",");
+	while (token != NULL) {
+		printf("%s\n", token);
+		token = strtok(NULL, ",");
+	} 
+
+
+    munmap(mapped_mem, size);
+    close(fd);
+    shm_unlink(shmname);  // Elimina el segmento de memoria compartida cuando ya no se necesita
+
+    return 0;
 }
